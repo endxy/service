e514a5f396cdc5c953cdccb407d5da155b34a34a
MJB9595 <wansja7870@gmail.com>
2026-01-28 15:46:34 +0900

    index page updated(processing)

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".gallery", {
+    pagination: {
+        el: ".gallery .swiper-pagination",
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    loop:true,
+    // autoplay:true
+    }); 
+
